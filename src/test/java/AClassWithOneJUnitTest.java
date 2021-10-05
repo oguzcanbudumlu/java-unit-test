4d38e7f8e6b0d983cd92e501035d4c9d7ac1d70e
Oğuzcan Budumlu <obudumlu@gmail.com>
2021-10-05 17:48:44 +0300

    Initial commit

---
+++ src/test/java/AClassWithOneJUnitTest.java
@@ -0,0 +1,9 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class AClassWithOneJUnitTest {
+    @Test
+    void demoTestMethod() {
+        assertTrue(true);
+    }
+}
